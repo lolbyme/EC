f16525d298399b8f226459d5829212ce6c5ae15d
lolbyme <32265064+lolbyme@users.noreply.github.com>
2017-10-02 19:01:28 +0200

    aa
    a

--- src/test/java/TestBankAccount.java
+++ src/test/java/TestBankAccount.java
@@ -11,4 +11,12 @@ public class TestBankAccount {
        double amount = account.debit(5);
        Assert.assertEquals(5.0, amount);
     }
+
+    @Test
+    public void testFunds2() {
+       BankAccount account = new BankAccount(10);
+       double amount = account.debit(10);
+       Assert.assertEquals(10.0, amount);
+    }
+
 }
