9c54a7a907b26be3cd25c75b81af7f96bf6b4845
lolbyme <32265064+lolbyme@users.noreply.github.com>
2017-12-13 19:13:31 +0100

    a
    a

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -4,9 +4,13 @@ public class FizzBuzz {
     public String getFizzBuzzWord(int number) {
         StringBuilder fizzBuzzWord = new StringBuilder();
 
-        /*if (number % 3 == 0) {
+        if (number % 3 == 0) {
             fizzBuzzWord.append("Fizz");
-        }*/
+        }
+
+        if (number % 4 == 0) {
+            fizzBuzzWord.append("lal");
+        }
 
         if (number % 5 == 0) {
             fizzBuzzWord.append("Buzz");
