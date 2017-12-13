84b947e0caca9b2403a877cecc3ccf5727c70ea4
lolbyme <32265064+lolbyme@users.noreply.github.com>
2017-12-13 18:54:40 +0100

    Test
    Tes Commit

--- src/integration-test/java/ITFizzBuzzTest.java
+++ src/integration-test/java/ITFizzBuzzTest.java
@@ -7,7 +7,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 /**
- * @author Petri Kainulainen
+ * @author Zsolt Dunai
  */
 public class ITFizzBuzzTest {
 
